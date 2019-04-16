a3eea5c825d3119724412d19cc197a7cf3fe1181
gustavo.carvalho <gustavo.carvalho@mprj.mp.br>
2019-04-16 17:42:35 -0300

    ajuste layout

--- src/app/core/criar-caso/criar-caso.component.ts
+++ src/app/core/criar-caso/criar-caso.component.ts
@@ -9,14 +9,16 @@ import {FormControl} from '@angular/forms';
 export class CriarCasoComponent implements OnInit {
   selectedValueAssignment: string ;
   selectedValueTheme: string ;
+  selectedValueDestiny: string ;
 
 
   assignment = new FormControl();
   theme = new FormControl();
+  destiny = new FormControl();
 
   assignmentList = ['atribuicao_1', 'atribuicao_2', 'atribuicao_3', 'atribuicao_3', 'atribuicao_3', 'atribuicao_3'];
   themeList = ['tema_1', 'tema_2', 'tema_3', 'tema_4', 'tema_5', 'tema_6'];
-
+  destinyList = ['destinatario_1', 'destinatario_2', 'destinatario_3', 'destinatario_4', 'destinatario_5', 'destinatario_6'];
   constructor() { }
 
   ngOnInit() {
